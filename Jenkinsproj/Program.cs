ce7d0d1ab0dd4a626b22a428872fc0211e39117d
git-Yamminipathy16 <yamminipathy2000@gmail.com>
2021-07-29 10:13:53 +0530

    Add project files.

---
+++ Jenkinsproj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkinsproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
